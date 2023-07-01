40b402f715899b122ae9fc9e66406b37461a8986
Symon  Kipkemei <85394343+symonkipkemei@users.noreply.github.com>
2023-07-01 20:15:52 +0300

    updates on pushbutton

--- panelization.tab/panel.panel/MultiPanel.pushbutton/script.py
+++ panelization.tab/panel.panel/MultiPanel.pushbutton/script.py
@@ -1,30 +1,40 @@
 from __future__ import division
 
 # METADATA
-################################################################################################################################
-
 
 __title__ = "MultiPanel"
 
-__doc__ = """ Version  1.1
+__doc__ = """Version  1.3
 Date  = 09.06.2023
-___________________________________________________________________________
+___________________________________________________________
 Description:
 
-This tool will auto place reveals in a selected wall :
+This tool will create multiple panel of 4' or less 
+by breaking down a Part into panels 
 
-___________________________________________________________________________
+Direction based on type of the Parts
+
+Exterior Parts  -> left to right
+Interior Parts  -> right to left
+Partition Parts -> right to left
+
+Laps based on type of Parts
+Exterior Parts  -> right lap
+Interior Parts  -> left lap
+Partition Parts -> left lap
+
+___________________________________________________________
 How-to:
-
 -> Click on the button
-___________________________________________________________________________
+-> Select a Part
+___________________________________________________________
 last update:
-- [09.06.2023] - 1.0 RELEASE
+- [01.07.2023] - 1.3 RELEASE
 
-___________________________________________________________________________
-To-Do:
--> Develop the abstract tool
-___________________________________________________________________________
+___________________________________________________________
+To do:
+-> Allow the user to set a constrain of the smallest panel size
+___________________________________________________________
 Author: Symon Kipkemei
 
 """
@@ -40,36 +50,15 @@ __max_revit_ver__ = 2022
 # IMPORTS
 ################################################################################################################################
 
-
-# regular
-
-import random
-
-# Autodesk
 from Autodesk.Revit.DB import *
-from Autodesk.Revit.DB import Transaction, Element, ElementId, FilteredElementCollector
-from Autodesk.Revit.DB.Structure import StructuralType
-from Autodesk.Revit.UI.Selection import ObjectType
-# pyrevit
-
-
-# custom ( Remember to include the csutom lib package to the pythonpath)
-
-from _create import _annotations as an
-
-# .NET imports ( I have no idea why I am importing this)
 import clr
 
 clr.AddReference("System")
-from System.Collections.Generic import \
-    List  # List<ElementType>() <-it's special type of list that RevitAPI often requires
+from _create import _auto as a
+from _create import _get as g
 
 # VARIABLES
 ################################################################################################################################
-
-
-# instance variables from revitAPI
-
 
 # __revit__  used to create an instance
 app = __revit__.Application  # represents the Revit Autodesk Application
@@ -81,158 +70,39 @@ active_view = doc.ActiveView
 active_level = doc.ActiveView.GenLevel
 
 
-def place_reveal(host_wall_id, variable_distance):
-    # get part, this is the hosted element containing the part
-    wall = doc.GetElement(host_wall_id)
-    # get symbol
-    wallSweepType = WallSweepType.Reveal
-    wallSweepTypeId = ElementId(352808)
+def main():
+    part = g.get_part()
+    host_wall_id = g.get_host_wall_id(part)
+    layer_index = g.get_layer_index(part)
+    left_lap_id  = ElementId(352818)
+    right_lap_id = ElementId(352808)
 
-    wallSweepInfo = WallSweepInfo(wallSweepType, True)
-    wallSweepInfo.CutsWall = True
-    wallSweepInfo.Distance = variable_distance
-    wallSweepInfo.WallSide = WallSide.Exterior
-    wallSweepInfo.DistanceMeasuredFrom = DistanceMeasuredFrom.Base
-    wall_sweep = WallSweep.Create(wall, wallSweepTypeId, wallSweepInfo)
+    variable_distance = 3
 
-    return wall_sweep
+    if layer_index == 1:  # exterior face
+        side_of_wall = WallSide.Exterior
+        lap_type_id = right_lap_id
+        left_edge, right_edge = g.get_edge_index(__title__, part, lap_type_id,variable_distance, side_of_wall)
+        reveal_indexes = g.get_reveal_indexes(left_edge, right_edge, exterior_face=True)
+        a.auto_panel(__title__, host_wall_id, lap_type_id, reveal_indexes, side_of_wall)
 
+    elif layer_index == 2:  # interior face of partition walls
+        side_of_wall = WallSide.Interior
+        lap_type_id = left_lap_id
+        left_edge, right_edge = g.get_edge_index(__title__, part, lap_type_id, variable_distance, side_of_wall)
+        reveal_indexes = g.get_reveal_indexes(left_edge, right_edge, exterior_face=False)
+        a.auto_panel(__title__, host_wall_id, lap_type_id, reveal_indexes, side_of_wall)
 
-
-def get_host_wall_id(part):
-    """
-    Returns the host wall id when a par element is selected
-
-    :param part_element_id:
-    :return:
-    """
-
-    linkedElementIdCollection = part.GetSourceElementIds()
-
-    host_wall_id = linkedElementIdCollection[0].HostElementId  # pick first linkedelement in collection
-
-    return host_wall_id
-
-
-def get_part():
-    # select part element
-    reference = uidoc.Selection.PickObject(ObjectType.Element)
-    part = uidoc.Document.GetElement(reference)
-
-    return part
-
-
-def get_edge_index(part):
-    """
-    Abstract the distance between the left edge and reveal postioned at o
-    :return: part_length and part_distance_origin
-    """
-    try:
-
-        # abstract the length of the part
-        part_length = part.get_Parameter(BuiltInParameter.DPART_LENGTH_COMPUTED).AsDouble()
-
-        half_part_length = part_length/2
-
-        # place reveal
-        host_wall_id = get_host_wall_id(part)
-        variable_distance = 1
-
-        with Transaction(doc, __title__) as t:
-            t.Start()
-            wall_sweep = place_reveal(host_wall_id, variable_distance)
-            t.Commit()
-
-        # get new_part_length
-        new_part_length = part.get_Parameter(BuiltInParameter.DPART_LENGTH_COMPUTED).AsDouble()
-
-        #determine the panel size
-        left_edge_index = 0
-        right_edge_index = 0
-
-        if new_part_length < half_part_length:
-            # get distance between the left edge and wallSweepInfo.Distance at 0
-            left_edge_index = (part_length - new_part_length) + variable_distance
-            right_edge_index = left_edge_index - part_length
-
-        elif new_part_length > half_part_length:
-            left_edge_index = new_part_length + variable_distance
-            right_edge_index = left_edge_index - part_length
-
-        # delete reveal after abstracting the length
-        with Transaction(doc, __title__) as t:
-            t.Start()
-            doc.Delete(wall_sweep.Id)
-            t.Commit()
-
-        return left_edge_index, right_edge_index
-
-    except Exception as e:
-        print ('The following error has occurred: {}'.format(e))
-
-        return None
-
-
-
-
-def get_reveal_indexes(left_edge, right_edge):
-    # place reveal at correct position
-    part_length = left_edge - right_edge
-    panel_size = 3.927083 #3' 11 1/8"
-    half_panel_size = 1.927083 #1' 11 1/8"
-
-    no_complete_panels = int(part_length // panel_size)
-    incomplete_panel_length = part_length % panel_size
-
-    reveal_indexes = []
-
-    if incomplete_panel_length == 0:
-        for x in range(0, no_complete_panels):
-            left_edge -= panel_size
-            reveal_indexes.append(left_edge)
-
-    elif incomplete_panel_length < 2:
-        # If there is an incomplete panel half the panel before the last reveal
-        for x in range(0, (no_complete_panels - 1)):
-            left_edge -= panel_size
-            reveal_indexes.append(left_edge)
-        left_edge -= half_panel_size
-        reveal_indexes.append(left_edge)
-
-    elif incomplete_panel_length > 2:
-        # If there is an incomplete panel half the panel before the last reveal
-        for x in range(0, no_complete_panels):
-            left_edge -= panel_size
-            reveal_indexes.append(left_edge)
+    elif layer_index == 3:  # interior face
+        side_of_wall = WallSide.Interior
+        lap_type_id = left_lap_id
+        left_edge, right_edge = g.get_edge_index(__title__, part, lap_type_id, variable_distance, side_of_wall)
+        reveal_indexes = g.get_reveal_indexes(left_edge, right_edge, exterior_face=False)
+        a.auto_panel(__title__, host_wall_id, lap_type_id, reveal_indexes, side_of_wall)
 
     else:
-        print ("The incomplete panel length is unknown or faulty")
-
-    # check if there is a remainder
-    return reveal_indexes
-
-
-def auto_panel(host_wall_id, reveal_indexes):
-    # place new reveal
-    try:
-        with Transaction(doc, __title__) as t:
-            t.Start()
-
-            for reveal_index in reveal_indexes:
-                wall_sweep = place_reveal(host_wall_id, reveal_index)
-
-            t.Commit()
-    except Exception as e:
-        print ('The following error has occurred: {}'.format(e))
-
-
-def main():
-    part = get_part()
-    host_wall_id = get_host_wall_id(part)
-    left_edge_index, right_edge_index = get_edge_index(part)
-    reveal_indexes = get_reveal_indexes(left_edge_index, right_edge_index)
-
-    auto_panel(host_wall_id, reveal_indexes)
+        print ("This is a foreign Part not recognized, "
+               "raise an issue on 'https://github.com/symonkipkemei/panelization/issues' ")
 
 
 if __name__ == "__main__":
--- panelization.tab/panel.panel/SinglePanel.pushbutton/script.py
+++ panelization.tab/panel.panel/SinglePanel.pushbutton/script.py
@@ -1,17 +1,24 @@
-
-
 # METADATA
-################################################################################################################################
+__title__ = "SinglePanel"
 
-
-__title__ = "AutoReveal"
-
-__doc__ = """ Version  1.1
+__doc__ = """Version  1.3
 Date  = 09.06.2023
 ___________________________________________________________
 Description:
 
-This tool will auto place reveals on a selected Part
+This tool will create a single panel of 4' 
+based on the  type of the Parts
+
+Exterior Parts  -> left to right
+Interior Parts  -> right to left
+Partition Parts -> right to left
+
+The tool is able to intuitively identify the 
+type of parts and use the correct lap
+
+Exterior Parts  -> right lap
+Interior Parts  -> left lap
+Partition Parts -> left lap
 
 ___________________________________________________________
 How-to:
@@ -20,11 +27,8 @@ How-to:
 -> Select a Part
 ___________________________________________________________
 last update:
-- [01.07.2023] - 1.1 RELEASE
+- [01.07.2023] - 1.3 RELEASE
 
-___________________________________________________________
-To-Do:
--> a form to allow the user adjust the distance from 0
 ___________________________________________________________
 Author: Symon Kipkemei
 
@@ -32,9 +36,12 @@ Author: Symon Kipkemei
 
 __author__ = "Symon Kipkemei"
 __helpurl__ = "https://www.linkedin.com/in/symon-kipkemei/"
+
 __highlight__ = 'new'
+
 __min_revit_ver__ = 2020
 __max_revit_ver__ = 2023
+
 
 # IMPORTS
 
@@ -43,14 +50,13 @@ from Autodesk.Revit.DB import Transaction, Element, ElementId, FilteredElementCo
 from Autodesk.Revit.DB.Structure import StructuralType
 from Autodesk.Revit.UI.Selection import ObjectType
 
+from _create import _get as g
 from _create import _auto as a
-from _create import  _get as g
 
 import clr
 clr.AddReference("System")
 
 # VARIABLES
-
 app = __revit__.Application  # represents the Revit Autodesk Application
 doc = __revit__.ActiveUIDocument.Document  # obj used to create new instances of elements within the active project
 uidoc = __revit__.ActiveUIDocument  # obj that represent the current active project
@@ -67,23 +73,30 @@ def main():
     left_lap_id = ElementId(352818)
     right_lap_id = ElementId(352808)
 
-    variable_distance = 0
+    variable_distance = 3
 
     if layer_index == 1:  # exterior face
         side_of_wall = WallSide.Exterior
         lap_type_id = right_lap_id
-        a.auto_place_reveal(__title__, host_wall_id,lap_type_id,variable_distance, side_of_wall)
+        left_edge, right_edge = g.get_edge_index(__title__, part, lap_type_id, variable_distance, side_of_wall)
+        reveal_indexes = g.get_single_panel_reveal_indexes(left_edge, right_edge, exterior_face=True)
+        a.auto_panel(__title__, host_wall_id, lap_type_id, reveal_indexes, side_of_wall)
 
     elif layer_index == 2:  # interior face of partition walls
         side_of_wall = WallSide.Interior
         lap_type_id = left_lap_id
-        a.auto_place_reveal(__title__, host_wall_id, lap_type_id, variable_distance, side_of_wall)
+        left_edge, right_edge = g.get_edge_index(__title__, part, lap_type_id, variable_distance, side_of_wall)
+        reveal_indexes = g.get_single_panel_reveal_indexes(left_edge, right_edge, exterior_face=False)
+        a.auto_panel(__title__, host_wall_id, lap_type_id, reveal_indexes, side_of_wall)
 
     elif layer_index == 3:  # interior face
         side_of_wall = WallSide.Interior
         lap_type_id = left_lap_id
-        a.auto_place_reveal(__title__, host_wall_id, lap_type_id, variable_distance, side_of_wall)
+        left_edge, right_edge = g.get_edge_index(__title__, part, lap_type_id, variable_distance, side_of_wall)
+        reveal_indexes = g.get_single_panel_reveal_indexes(left_edge, right_edge, exterior_face=False)
+        a.auto_panel(__title__, host_wall_id, lap_type_id, reveal_indexes, side_of_wall)
 
 
 if __name__ == "__main__":
+    # print(get_part_length(496067))
     main()
